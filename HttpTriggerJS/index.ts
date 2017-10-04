90892be5a92304fae463e8916ef90cb7b25e0a9f
Tsuyoshi Ushio <ushio@simplearchitect.com>
2017-10-04 13:54:52 +0900

    Initial Commit

---
+++ HttpTriggerJS/index.ts
@@ -0,0 +1,26 @@
+import * as DB from "documentdb-typescript";
+
+
+export async function run (context: any, req: any) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const client = new DB.Client(process.env.COSMOS_DB_HOST, process.env.COSMOS_DB_KEY);
+    client.enableConsoleLog = true;
+    //  console.log(await client.getAccountInfoAsync());
+    await client.openAsync();
+    var dbs = await client.listDatabasesAsync();
+
+    if (req.query.name || (req.body && req.body.name)) {
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: "Hello! " + (req.query.name || req.body.name) + dbs.map(db => db.id)
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+    context.done();
+};
---
+++ HttpTriggerJS/memo.md
@@ -0,0 +1,18 @@
+
+func new
+cd HttpTriggerJS
+npm init
+tsc --init
+cp index.js index.ts
+
+
+```ts
+export function run (context: any, req: any): void {
+```
+
+tsc -p .
+
+func host start
+
+npm install documentdb-typescript --save
+
